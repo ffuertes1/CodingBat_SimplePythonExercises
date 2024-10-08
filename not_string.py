6e7ff5a7a7a57dbbc0e1176e84cb558af0cd7ca4
ffuertes1 <ffuertes25@gmail.com>
2024-10-08 08:27:26 -0700

    Conditionals_str[No loops]

---
+++ not_string.py
@@ -0,0 +1,12 @@
+# Given a string, return a new string where "not " 
+# has been added to the front. However, if the string 
+# already begins with "not", return the string unchanged.
+# ##
+
+def not_string(str):
+    if str.startswith("not"):
+        return str
+    else:
+        return("not " +str )
+x=not_string("not Hola Mundo")
+print(x)
