7437a405189da4b703a367dc060e8b5c931c6f4f
ffuertes25 <ffuertes25@gmail.com>
2024-10-13 20:33:39 -0400

    String Concadenating

---
+++ String-1/make_abba.py
@@ -0,0 +1,8 @@
+#Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+def make_abba(a, b):
+  return (a+b)+(b+a)
+
+x=make_abba("Hi","Bye")
+
+print (x)
